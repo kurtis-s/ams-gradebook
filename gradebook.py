a3243890372f579c5eef7043c0de92df9c85a2ce
kurtis-s <kurtis_s@hotmail.com>
2015-01-04 14:43:40 -0800

    refactored oauth2 process into a class

--- gradebook.py
+++ gradebook.py
@@ -6,47 +6,50 @@ from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client import tools
 
-SCOPE = ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds']
-CLIENT_SECRETS_JSON_PATH = 'client_secrets.json'
-CREDENTIALS_FILE_PATH = 'credentials_file'
+class Authorizor(object):
+    SCOPE = ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds']
+    CLIENT_SECRETS_JSON_PATH = 'client_secrets.json'
+    CREDENTIALS_FILE_PATH = 'credentials_file'
 
-def _refresh_token(credentials):
-    if credentials.access_token_expired:
-        http = httplib2.Http()
-        credentials.refresh(http)
+    def __init__(self):
+        storage = Storage(self.CREDENTIALS_FILE_PATH)
+        self.storage = storage
+        self.credentials = storage.get()
 
-    return credentials
+    def _refresh_token(self):
+        if self.credentials.access_token_expired:
+            http = httplib2.Http()
+            self.credentials.refresh(http)
+            self.storage.put(self.credentials)
 
-def _authorize_application(storage):
-    parser = argparse.ArgumentParser(parents=[tools.argparser])
-    flags = parser.parse_args()
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_JSON_PATH, scope=SCOPE)
+    def _authorize_application(self):
+        parser = argparse.ArgumentParser(parents=[tools.argparser])
+        flags = parser.parse_args()
+        flow = flow_from_clientsecrets(self.CLIENT_SECRETS_JSON_PATH, scope=SCOPE)
 
-    authorized_credentials = tools.run_flow(flow, storage, flags)
+        authorized_credentials = tools.run_flow(flow, self.storage, flags)
 
-    return authorized_credentials
+        return authorized_credentials
 
-def get_credentials():
-    """Returns an instantiated Credentials object.  Refreshes the Credential's
-    access token if the token is expired.
-    
-    If no existing credentials file is found, attempt to authorize the application
-    with Google.  After the application has been authorized, the new Credential is
-    stored in CREDENTIALS_FILE_PATH.
-    """
-    storage = Storage(CREDENTIALS_FILE_PATH)
-    credentials = storage.get()
+    def get_credentials(self):
+        """Returns an instantiated Credentials object.  Refreshes the Credential's
+        access token if the token is expired.
+        
+        If no existing credentials file is found, attempt to authorize the application
+        with Google.  After the application has been authorized, the new Credential is
+        stored in CREDENTIALS_FILE_PATH.
+        """
+        #storage.get() returns None if there is no credentials file
+        if self.credentials is None:
+            credentials = self._authorize_application(storage)
 
-    #storage.get() returns None if there is no credentials file
-    if credentials is None:
-        credentials = _authorize_application(storage)
+        self._refresh_token()
 
-    _refresh_token(credentials)
-
-    return credentials
+        return self.credentials
 
 if __name__=='__main__':
-    credentials = get_credentials()
+    authorizor = Authorizor()
+    credentials = authorizor.get_credentials()
     gc = gspread.authorize(credentials)
     #wks = gc.open("gradetestbook").Sheet1
     wks = gc.open_by_key('1SqrL7FigyTy9jhZ9pEDBYplRQaXnDf_Iz-8-MT1LN7o').sheet1
