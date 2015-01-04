f4cfa40a5d30134631ff57147ddc497408877b5a
kurtis-s <kurtis_s@hotmail.com>
2015-01-03 22:49:01 -0800

    first minimal working code using oauth2 and gspread

--- gradebook.py
+++ gradebook.py
@@ -1,33 +1,55 @@
-from credentials import CLIENT_ID, CLIENT_SECRET
-from oauth2client.client import OAuth2WebServerFlow
-from flask import Flask, request
-import requests
-from threading import Thread
-from urlparse import urlparse
+import argparse
+import gspread
+import httplib2
 
-REDIRECT_URI = 'http://localhost:5000' #Flask uses port 5000 by default
+from oauth2client.file import Storage
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client import tools
+
 SCOPE = ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds']
-USER_AGENT = 'ams-gradebook/1.0'
+CLIENT_SECRETS_JSON_PATH = 'client_secrets.json'
+CREDENTIALS_FILE_PATH = 'credentials_file'
 
-flaskapp = Flask(__name__)
-@flaskapp.route("/")
-def getauthcode():
-    print request.args.get('code')
-    return request.args.get('code')
+def _refresh_token(credentials):
+    if credentials.access_token_expired:
+        http = httplib2.Http()
+        credentials.refresh(http)
 
-flow = OAuth2WebServerFlow(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        scope=SCOPE,
-        user_agent=USER_AGENT)
+    return credentials
+
+def _authorize_application(storage):
+    parser = argparse.ArgumentParser(parents=[tools.argparser])
+    flags = parser.parse_args()
+    flow = flow_from_clientsecrets(CLIENT_SECRETS_JSON_PATH, scope=SCOPE)
+
+    authorized_credentials = tools.run_flow(flow, storage, flags)
+
+    return authorized_credentials
+
+def get_credentials():
+    """Returns an instantiated Credentials object.  Refreshes the Credential's
+    access token if the token is expired.
+    
+    If no existing credentials file is found, attempt to authorize the application
+    with Google.  After the application has been authorized, the new Credential is
+    stored in CREDENTIALS_FILE_PATH.
+    """
+    storage = Storage(CREDENTIALS_FILE_PATH)
+    credentials = storage.get()
+
+    #storage.get() returns None if there is no credentials file
+    if credentials is None:
+        credentials = _authorize_application(storage)
+
+    _refresh_token(credentials)
+
+    return credentials
 
 if __name__=='__main__':
-    flask_thread = Thread(target=flaskapp.run)
-    flask_thread.daemon = True
-    flask_thread.start()
-    auth_uri = flow.step1_get_authorize_url()
-    auth_response = requests.get(auth_uri)
-    if 'accounts.google.com' == urlparse(auth_response.history[-1].url).netloc:
-        print "Please authorize the app here:\n{}".format(auth_response.history[-1].url)
-    print auth_response.url
+    credentials = get_credentials()
+    gc = gspread.authorize(credentials)
+    #wks = gc.open("gradetestbook").Sheet1
+    wks = gc.open_by_key('1SqrL7FigyTy9jhZ9pEDBYplRQaXnDf_Iz-8-MT1LN7o').sheet1
+    print "value in B2"
+    print wks.acell('B2').value
+    print "done"
