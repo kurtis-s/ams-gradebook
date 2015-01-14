ebe2952dc223fc6ad78cb92b53dde29c1d14e2f1
kurtis-s <kurtis_s@hotmail.com>
2015-01-13 20:14:08 -0800

    spreadsheetconfig in gitignore

--- spreadsheetconfig.py
+++
@@ -1,7 +0,0 @@
-#This is a configuration parameter used to set the gradebook's Google sheets key.  
-#
-#See https://developers.google.com/apps-script/reference/spreadsheet/spreadsheet-app#openById%28String%29
-#for details
-key = "abc1234567"
-first_name_column_header = "First Name"
-last_name_column_header = "Last Name"
